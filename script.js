19ad131525c69e3339ac86cd2560c83a2f782df9
Amanda Guerra <guerraamanda1@hotmail.com>
2023-09-22 11:49:36 -0300

    lógica de ganhar em todas as posições e empate

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let newGameButton = document.querySelector('.new-game');
 let resetGameButton = document.querySelector('.reset-game');
 let gameOverFlag = document.querySelector('#game-over-flag');
 let winnerMsg = document.querySelector('#winner-msg');
-let line = document.querySelector('.line');
+let line = document.querySelectorAll('.line');
+let lineH = document.querySelector('.line-h');
+let lineV = document.querySelector('.line-v');
+let lineDl = document.querySelector('.line-dl');
+let lineDr = document.querySelector('.line-dr');
 
 let position1 = document.querySelector('.position1');
 let position2 = document.querySelector('.position2');
@@ -49,9 +53,23 @@ function resetGame() {
   newGameButton.classList.remove('hide');
   resetGameButton.classList.add('hide');
   gameOverFlag.classList.add('hide');
-  line.style.border = 'none';
+
+  resetCrossLine();
+
   setLocalStorage('@tic-tac-toe: isGameStarted', false);
   disabledOrAble('able');
+}
+
+function resetCrossLine() {
+  for (let i = 0; i < line.length; i++) {
+    let divClassList = line[i].classList
+    for (let i = 0; i < divClassList.length; i++) {
+      let divClass = divClassList[i];
+      if(divClass !== "line" && divClass !== "line-h" && divClass !== 'line-dl' && divClass !== 'line-v' && divClass !== 'line-dr') {
+        divClassList.remove(divClass);
+      }
+    }
+  }
 }
 
 function newGame() {
@@ -104,55 +122,119 @@ function play(position) {
   }
 }
 
-function lineHorizontally(player, position) {
+function putCrossLine(player, position) {
   let playerX = player === 'X';
   let playerO = player === 'O';
-  line.removeAttribute('style');
 
-  let firstLine = position === 'first line';
-  let middleLine = position === 'middle line';
-  let lastLine = position === 'last line';
+  let firstLineH = position === 'first line h';
+  let middleLineH = position === 'middle line h';
+  let lastLineH = position === 'last line h';
 
-  console.log(playerX || playerO);
-  console.log(middleLine);
+  let firstLineV = position === 'first line v';
+  let middleLineV = position === 'middle line v';
+  let lastLineV = position === 'last line v';
 
-  if ((playerX || playerO) && firstLine) {
-    line.classList.add('first-line-h');
-  } else if ((playerX || playerO) && middleLine) {
-    line.classList.add('middle-line-h');
-  } else if ((playerX || playerO) && lastLine) {
-    line.classList.add('last-line-h');
+  let diagonalLeft = position === 'diagonal left';
+  let diagonalRight = position === 'diagonal right';
+
+  if ((playerX || playerO) && firstLineH) {
+    lineH.classList.add('first-line-h');
+    console.log("ganhou na primeira linha horizontalmente")
+  } else if ((playerX || playerO) && middleLineH) {
+    lineH.classList.add('middle-line-h');
+  } else if ((playerX || playerO) && lastLineH) {
+    lineH.classList.add('last-line-h');
+  } else if ((playerX || playerO) && firstLineV) {
+    lineV.classList.add('first-line-v');
+    console.log("ganhou na primeira linha verticalmente")
+  } else if ((playerX || playerO) && middleLineV) {
+    lineV.classList.add('middle-line-v');
+  } else if ((playerX || playerO) && lastLineV) {
+    lineV.classList.add('last-line-v');
+  } else if ((playerX || playerO) && diagonalLeft) {
+    lineDl.classList.add('diagonal-left');
+  } else if ((playerX || playerO) && diagonalRight) {
+    lineDr.classList.add('diagonal-right');
   } else {
     console.log('entrou aqui');
   }
 }
 
-function wonHorizontally(player, position) {
-  let firstLine =
+function checkIfWonHorizontally(player) {
+  let firstLineH =
     position1.textContent === player &&
     position2.textContent === player &&
     position3.textContent === player;
-  let middleLine =
+  let middleLineH =
     position4.textContent === player &&
     position5.textContent === player &&
     position6.textContent === player;
-  let lastLine =
+  let lastLineH =
     position7.textContent === player &&
     position8.textContent === player &&
     position9.textContent === player;
 
-  if (firstLine) {
-    lineHorizontally(player, 'first line');
+  if (firstLineH) {
+    putCrossLine(player, 'first line h');
     return true;
-  } else if (middleLine) {
-    lineHorizontally(player, 'middle line');
+  } else if (middleLineH) {
+    putCrossLine(player, 'middle line h');
     return true;
-  } else if (lastLine) {
-    lineHorizontally(player, 'last line');
-    return true
+  } else if (lastLineH) {
+    putCrossLine(player, 'last line h');
+    return true;
   } else {
-    return false
+    return false;
   }
+}
+
+function checkIfWonVertically(player) {
+  let firstLineV =
+    position1.textContent === player &&
+    position4.textContent === player &&
+    position7.textContent === player;
+  let middleLineV =
+    position2.textContent === player &&
+    position5.textContent === player &&
+    position8.textContent === player;
+  let lastLineV =
+    position3.textContent === player &&
+    position6.textContent === player &&
+    position9.textContent === player;
+
+  if (firstLineV) {
+    putCrossLine(player, 'first line v');
+    return true;
+  } else if (middleLineV) {
+    putCrossLine(player, 'middle line v');
+    return true;
+  } else if (lastLineV) {
+    putCrossLine(player, 'last line v');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkIfWonDiagonally(player) {
+  let diagonalLeft =
+    position1.textContent === player &&
+    position5.textContent === player &&
+    position9.textContent === player;
+  let diagonalRight =
+    position3.textContent === player &&
+    position5.textContent === player &&
+    position7.textContent === player;
+
+    if (diagonalLeft) {
+      putCrossLine(player, 'diagonal left');
+      return true;
+    } else if (diagonalRight) {
+      putCrossLine(player, 'diagonal right');
+      return true;
+    } else {
+      return false;
+    }
 }
 
 function gameOver(player) {
@@ -162,26 +244,49 @@ function gameOver(player) {
     XturnButton.classList.add('hide');
   }
 
+  if(player === 'X' || player === 'O') {
+    winnerMsg.textContent = `${player} won!`;
+  } else {
+    winnerMsg.textContent = "It's a draw!";
+    XturnButton.classList.add('hide');
+    OturnButton.classList.add('hide');
+  }
+
   gameOverFlag.classList.remove('hide');
-  winnerMsg.textContent = `${player} won!`;
   newGameButton.classList.remove('hide');
   resetGameButton.classList.add('hide');
 
   disabledOrAble('disabled');
 }
 
+function allPositionsAreFilled() {
+  let positionsFilled = [];
+  for (let i = 1; i <= 9; i++) {
+    let position = '.position' + i;
+    let currentPosition = document.querySelector(position).textContent
+
+    if (currentPosition === 'X' || currentPosition === 'O') {
+      positionsFilled.push({position: true})
+    } else {
+      positionsFilled.push({position: false})
+    }    
+  }
+
+  for (let i = 0; i < positionsFilled.length; i++) {
+    if(positionsFilled[i].position === false) {
+      return false
+    };
+  }
+
+  return true;
+}
+
 function checkWhoWon() {
-  if (
-    wonHorizontally('X', 'first line') ||
-    wonHorizontally('X', 'middle line') ||
-    wonHorizontally('X', 'last line')
-  ) {
+  if (checkIfWonHorizontally('X') || checkIfWonVertically('X') || checkIfWonDiagonally('X')) {
     gameOver('X');
-  } else if (
-    wonHorizontally('O', 'first line') ||
-    wonHorizontally('O', 'middle line') ||
-    wonHorizontally('O', 'last line')
-  ) {
+  } else if (checkIfWonHorizontally('O') || checkIfWonVertically('O') || checkIfWonDiagonally('O')) {
     gameOver('O');
+  } else if(allPositionsAreFilled()) {
+    gameOver('draw')
   }
 }
